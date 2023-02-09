2219d8f1b5b4495086ddd31ef072559a65705733
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2023-02-09 18:43:34 +0200

    Add local urls.py for catalog app. Add index url and index view in views.py. Add indax.html template

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from catalog.models import Band, Musician, Genre, Instrument
+
+
+def index(request):
+    num_bands = Band.objects.count()
+    num_musicians = Musician.objects.count()
+    num_genres = Genre.objects.count()
+    num_instruments = Instrument.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_bands": num_bands,
+        "num_musicians": num_musicians,
+        "num_genres": num_genres,
+        "num_instruments": num_instruments,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "catalog/index.html", context=context)
