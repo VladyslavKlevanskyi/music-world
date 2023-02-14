2adb7292cc7298a32f8adbebac3a001c10f5240b
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2023-02-14 17:01:27 +0200

    Install django debug toolbar

--- music_world/urls.py
+++ music_world/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
