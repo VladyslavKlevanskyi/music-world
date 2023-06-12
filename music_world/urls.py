629dc57df76f7c22c08313cf2be70c197a1cec3e
Vladyslav Klevanskyi <vladislav.klevanskiy@gmail.com>
2023-06-12 16:54:31 +0300

    Authentication. Add url into the urls.py

--- music_world/urls.py
+++ music_world/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("catalog.urls", namespace="catalog")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
